89f03c2b46f4458fc275de22f67fe9dea0d977c8
Marco <vogel.marco@mailbox.org>
2024-10-05 17:49:49 +0200

    added sve example code

--- lib/include/base64sve.h
+++ lib/include/base64sve.h
@@ -1,3 +1,6 @@
 #include <stdio.h>
+#include <arm_sve.h>
 
 void test(void);
+
+void vec_add_sve_intrinsics(int *restrict out, int *restrict a, int *restrict b, int length);
--- lib/src/base64sve.c
+++ lib/src/base64sve.c
@@ -3,3 +3,19 @@
 void test(void){
     printf("Hello\n");
 }
+
+void vec_add_sve_intrinsics(int *restrict out, int *restrict a, int *restrict b, int length)
+{
+    svbool_t predicate;
+    svint32_t a_vec;
+    svint32_t b_vec;
+
+    for (int i = 0; i < length; i += svcntw())
+    {
+        predicate = svwhilelt_b32(i, length);
+        a_vec = svld1(predicate, &a[i]);
+        b_vec = svld1(predicate, &b[i]);
+        a_vec = svadd_z(predicate, a_vec, b_vec);
+        svst1(predicate, &out[i], a_vec);
+    }
+}
