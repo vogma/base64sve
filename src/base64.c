673baeff977a5309429490b05684775a4e162ece
Marco <vogel.marco@mailbox.org>
2024-10-08 11:02:52 +0200

    main

--- src/base64.c
+++ src/base64.c
@@ -1,52 +1,34 @@
 #include <base64sve.h>
 
-void printArray(uint32_t *input, int length)
+void printArray(uint8_t *input, int length)
 {
 
-    printf("\n\nhex: ");
+    // printf("hex: ");
     for (int i = 0; i < length; i++)
     {
-        printf("0x%08X ", input[i]);
+        printf("0x%02X ", input[i]);
     }
     printf("\n");
 
-    printf("dec: ");
-    for (int i = 0; i < length; i++)
-    {
-        printf("%d ", input[i]);
-    }
-    printf("\n");
+    // printf("dec: ");
+    // for (int i = 0; i < length; i++)
+    // {
+    //     printf("%d ", input[i]);
+    // }
+    // printf("\n");
 }
 
-void printArray16(uint16_t *input, int length)
-{
-
-    printf("\n\nhex: ");
-    for (int i = 0; i < length; i++)
-    {
-        printf("0x%04X ", input[i]);
-    }
-    printf("\n");
-
-    printf("dec: ");
-    for (int i = 0; i < length; i++)
-    {
-        printf("%d ", input[i]);
-    }
-    printf("\n");
-}
-
-#define N 6
+#define N 34
 
 int main(void)
 {
-    uint32_t a[N] = {0xA0000000, 0x00010001, 0x2, 0x4, 0xC, 0x2};
-    uint32_t c_sve_intr[N * 2] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
-    printArray(a, N);
-    testShiftingByMultiplication(a, c_sve_intr, N);
-    // testSaturatingSubstraction(a,c_sve_intr,N);
-    printArray(c_sve_intr, N );
+    uint8_t data[N] = {0x01, 0x02, 0x03, 0x04, 0x01, 0x02, 0x03, 0x04, 0x01, 0x02, 0x03, 0x04, 0x01, 0x02, 0x03, 0x04, 0x01, 0x02, 0x03, 0x04, 0x01, 0x02, 0x03, 0x04, 0x01, 0x02, 0x03, 0x04, 0x01, 0x02, 0x03, 0x04, 0XFA};
+    uint8_t output[N]={0};
+
+    printArray(data, N);
+    base64_encode(data, output, N);
+    printArray(output, N);
 
     return 0;
 }
