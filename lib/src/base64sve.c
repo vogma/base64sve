8d88c2ee4181ca51042ec7c9f471804b48eb075d
Marco <vogel.marco@mailbox.org>
2024-10-08 11:02:42 +0200

    added base64_encode foundation

--- lib/src/base64sve.c
+++ lib/src/base64sve.c
@@ -5,6 +5,51 @@ void test(void)
     printf("Hello\n");
 }
 
+void printRegister(svuint8_t vec)
+{
+    size_t bytes_per_vec = svcntb();
+    uint8_t memory[bytes_per_vec];
+
+    svbool_t predicate = svwhilelt_b8(0, (int)bytes_per_vec);
+    svst1(predicate, memory, vec);
+
+    printf("register contents: ");
+    for (int i = 0; i < bytes_per_vec; i++)
+    {
+        printf("0x%02X ", memory[i]);
+    }
+    printf("\n");
+}
+
+void base64_encode(void *input, char *output, size_t length)
+{
+    size_t bytes_per_vec = svcntb();
+    size_t bytes_per_round = (bytes_per_vec / 4) * 3;
+
+    const uint8_t encode_lookup_table[] = {1, 0, 2, 1, 4, 3, 5, 4, 7, 6, 8, 7, 10, 9, 11, 10, 13, 12, 14, 13, 16, 15, 17, 16, 19, 18, 20, 19, 22, 21, 23, 22, 25, 24, 26, 25, 28, 27, 29, 28, 31, 30, 32, 31, 34, 33, 35, 34, 37, 36, 38, 37, 40, 39, 41, 40, 43, 42, 44, 43, 46, 45, 47, 46, 49, 48, 50, 49, 52, 51, 53, 52, 55, 54, 56, 55, 58, 57, 59, 58, 61, 60, 62, 61};
+
+    svbool_t predicate8 = svwhilelt_b8(0, (int)bytes_per_round);
+    svbool_t predicateMax = svwhilelt_b8(0, (int)bytes_per_vec);
+
+    svuint8_t vec = svld1(predicate8, (uint8_t *)input);
+    svuint8_t vec_lookup_table = svld1(predicateMax, encode_lookup_table);
+
+    printRegister(vec);
+
+    vec = svtbl(vec, vec_lookup_table);
+
+    printRegister(vec);
+
+    svst1(predicate8, (int8_t *)output, svreinterpret_s8(vec));
+
+    // for (int i = 0; i < length - bytes_per_vec; i += bytes_per_vec, output += bytes_per_vec, input += bytes_per_vec)
+    // {
+    // svbool_t predicate8 = svwhilelt_b8(0, (int)length);
+    // svuint8_t vec = svld1(predicate8, (uint8_t *)input);
+    // svst1(predicate8, output, vec);
+    // }
+}
+
 void testShiftingByMultiplication(uint32_t *input, uint32_t *output, size_t length)
 {
     svbool_t predicate32 = svwhilelt_b32(0, (int)length);
@@ -13,7 +58,7 @@ void testShiftingByMultiplication(uint32_t *input, uint32_t *output, size_t leng
 
     svbool_t predicate16 = svwhilelt_b16(0, (int)length);
 
-    svuint32_t vec_constants = svdup_u32(0x00020004);
+    svuint32_t vec_constants = svdup_u32(0x00080020);
 
     // svuint32_t vec_constants = svdupq_n_u32(0x00800004,0x00800004,0x00800004,0x00800004);
     // svuint16_t vec_constants = svdup_u16(0x0080);
@@ -22,7 +67,6 @@ void testShiftingByMultiplication(uint32_t *input, uint32_t *output, size_t leng
 
     // svst1_u16(predicate, output, svreinterpret_u16(vec_constants));
     svst1(predicate32, output, svreinterpret_u32(result));
-
 }
 
 void testSaturatingSubstraction(uint8_t *input, uint8_t *output, size_t length)
