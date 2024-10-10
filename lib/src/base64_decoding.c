2f9d1008017e03c8e3e93b5c7369de432557ae9a
Marco <vogel.marco@mailbox.org>
2024-10-10 18:23:14 +0200

    added decoded tail handling

--- lib/src/base64_decoding.c
+++ lib/src/base64_decoding.c
@@ -1,6 +1,10 @@
 #include <base64sve.h>
 #include <limits.h>
 
+#define BASE64_PAD '='
+#define BASE64DE_FIRST '+'
+#define BASE64DE_LAST 'z'
+
 const int8_t LOWER_INVALID = 1;
 const int8_t UPPER_INVALID = 1;
 
@@ -23,44 +27,107 @@ static const int8_t shift_lut[16] = {
     /* 8 */ 0x00, /* 9 */ 0x00, /* a */ 0x00, /* b */ 0x00,
     /* c */ 0x00, /* d */ 0x00, /* e */ 0x00, /* f */ 0x00};
 
+/* ASCII order for BASE 64 decode, 255 in unused character */
+static const unsigned char base64de[] = {
+    /* nul, soh, stx, etx, eot, enq, ack, bel, */
+    255, 255, 255, 255, 255, 255, 255, 255,
+
+    /*  bs,  ht,  nl,  vt,  np,  cr,  so,  si, */
+    255, 255, 255, 255, 255, 255, 255, 255,
+
+    /* dle, dc1, dc2, dc3, dc4, nak, syn, etb, */
+    255, 255, 255, 255, 255, 255, 255, 255,
+
+    /* can,  em, sub, esc,  fs,  gs,  rs,  us, */
+    255, 255, 255, 255, 255, 255, 255, 255,
+
+    /*  sp, '!', '"', '#', '$', '%', '&', ''', */
+    255, 255, 255, 255, 255, 255, 255, 255,
+
+    /* '(', ')', '*', '+', ',', '-', '.', '/', */
+    255, 255, 255, 62, 255, 255, 255, 63,
+
+    /* '0', '1', '2', '3', '4', '5', '6', '7', */
+    52, 53, 54, 55, 56, 57, 58, 59,
+
+    /* '8', '9', ':', ';', '<', '=', '>', '?', */
+    60, 61, 255, 255, 255, 255, 255, 255,
+
+    /* '@', 'A', 'B', 'C', 'D', 'E', 'F', 'G', */
+    255, 0, 1, 2, 3, 4, 5, 6,
+
+    /* 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', */
+    7, 8, 9, 10, 11, 12, 13, 14,
+
+    /* 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', */
+    15, 16, 17, 18, 19, 20, 21, 22,
+
+    /* 'X', 'Y', 'Z', '[', '\', ']', '^', '_', */
+    23, 24, 25, 255, 255, 255, 255, 255,
+
+    /* '`', 'a', 'b', 'c', 'd', 'e', 'f', 'g', */
+    255, 26, 27, 28, 29, 30, 31, 32,
+
+    /* 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', */
+    33, 34, 35, 36, 37, 38, 39, 40,
+
+    /* 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', */
+    41, 42, 43, 44, 45, 46, 47, 48,
+
+    /* 'x', 'y', 'z', '{', '|', '}', '~', del, */
+    49, 50, 51, 255, 255, 255, 255, 255};
+
 static const uint8_t index_decode[] = {2, 1, 0, 6, 5, 4, 10, 9, 8, 14, 13, 12, 18, 17, 16, 22, 21, 20, 26, 25, 24, 30, 29, 28, 34, 33, 32, 38, 37, 36, 42, 41, 40, 46, 45, 44, 50, 49, 48, 54, 53, 52, 58, 57, 56, 62, 61, 60, 66, 65, 64, 70, 69, 68, 74, 73, 72, 78, 77, 76, 82, 81, 80, 86, 85, 84, 90, 89, 88, 94, 93, 92, 98, 97, 96, 102, 101, 100, 106, 105, 104, 110, 109, 108, 114, 113, 112, 118, 117, 116, 122, 121, 120, 126, 125, 124};
 
-void printRegister(svuint8_t vec)
+unsigned int base64_decode_tail(const char *in, unsigned int inlen, uint8_t *out)
 {
-    size_t bytes_per_vec = svcntb();
-    uint8_t memory[bytes_per_vec];
+    unsigned int i;
+    unsigned int j;
+    unsigned char c;
 
-    svbool_t predicate = svwhilelt_b8(0, (int)bytes_per_vec);
-    svst1(predicate, memory, vec);
-
-    printf("register contents: ");
-    for (int i = 0; i < bytes_per_vec; i++)
+    for (i = j = 0; i < inlen; i++)
     {
-        printf("%c ", memory[i]);
+        if (in[i] == BASE64_PAD)
+        {
+            break;
+        }
+        if (in[i] < BASE64DE_FIRST || in[i] > BASE64DE_LAST)
+        {
+            return 0;
+        }
+
+        c = base64de[(unsigned char)in[i]];
+        if (c == 255)
+        {
+            return 0;
+        }
+
+        switch (i & 0x3)
+        {
+        case 0:
+            out[j] = (c << 2) & 0xFF;
+            break;
+        case 1:
+            out[j++] |= (c >> 4) & 0x3;
+            out[j] = (c & 0xF) << 4;
+            break;
+        case 2:
+            out[j++] |= (c >> 2) & 0xF;
+            out[j] = (c & 0x3) << 6;
+            break;
+        case 3:
+            out[j++] |= c;
+            break;
+        }
     }
-    printf("\n");
-}
 
-void printRegister32(svuint32_t vec)
-{
-    size_t words_per_vec = svcntw();
-    uint32_t memory[words_per_vec];
-
-    svbool_t predicate = svwhilelt_b32(0, (int)words_per_vec);
-    svst1(predicate, memory, vec);
-
-    printf("register contents: ");
-    for (int i = 0; i < words_per_vec; i++)
-    {
-        printf("0x%08X ", memory[i]);
-    }
-    printf("\n");
+    return j;
 }
 
 void base64_decode(char *base64_data, void *output, size_t encoded_length, size_t *decoded_length)
 {
     size_t bytes_per_register = svcntb();
-    size_t output_bytes_per_round = (bytes_per_register / 3) * 4;
+    size_t output_bytes_per_round = (bytes_per_register / 4) * 3;
 
     // set needed predicates
     svbool_t predicateSave = svwhilelt_b8(0, (int)output_bytes_per_round);
@@ -70,7 +137,9 @@ void base64_decode(char *base64_data, void *output, size_t encoded_length, size_
     const svint8_t shift_lut_vec = svld1(predicate8Max, shift_lut);
     const svuint8_t index_decode_lut_vec = svld1(predicate8Max, index_decode);
 
-    for (; encoded_length >= bytes_per_register; encoded_length -= bytes_per_register, base64_data += bytes_per_register, output += output_bytes_per_round)
+    size_t output_length = 0;
+
+    for (; encoded_length - 1 >= bytes_per_register; encoded_length -= bytes_per_register, base64_data += bytes_per_register, output += output_bytes_per_round, output_length += output_bytes_per_round)
     {
         svint8_t data_vec = svld1(predicate8Max, (int8_t *)base64_data);
 
@@ -102,4 +171,10 @@ void base64_decode(char *base64_data, void *output, size_t encoded_length, size_
 
         svst1(predicateSave, (uint8_t *)output, result);
     }
+    if (encoded_length != 0)
+    {
+        output_length += base64_decode_tail(base64_data, encoded_length, (uint8_t *)output);
+    }
+
+    *decoded_length = output_length;
 }
